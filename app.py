2fda1f9c35a4eaab924df3739f9fde7ed81f795f
Arunpsrj <arunqualityengineer@gmail.com>
2026-04-25 18:42:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ Test Case:
 {description}
 """
 
-        model = genai.GenerativeModel("gemini-1.5-flash")
+        model = genai.GenerativeModel("gemini-1.0-pro")
         ai_response = model.generate_content(prompt)
 
         steps = ai_response.text
